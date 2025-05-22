f6d4a3301a9e0cc87448bc3755cccc1dcd93517d
Johanes Killan <killanalvesj@gmail.com>
2025-05-22 10:28:57 -0300

    Import and test fixes

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,6 @@
+import os
 import pytest
-import msgraph.msgraph as msgraph
+from msgraph.msgraph import Msgraph
 from unittest.mock import patch
 
 # This is the "tests" file for the project
@@ -9,7 +10,16 @@ from unittest.mock import patch
 def success_response(*args, **kwargs):
     class MockResponse:
         status_code = 200
-        def json(self): return {"access_token": "77777777777777777777777"}
+        content = bytes("TEST_CONTENT", "utf-8")
+        text = "TEST_CONTENT"
+        def json(self): return {
+            "access_token": "77777777777777777777777",
+            "value": [
+                {
+                    "id": "77777777777777777777777",
+                }
+            ]
+                                }
         @property
         def ok(self): return True
     return MockResponse()
@@ -22,6 +32,8 @@ def error_response(*args, **kwargs):
         @property
         def ok(self): return False
     return MockResponse()
+
+
 
 @pytest.fixture
 
@@ -39,120 +51,120 @@ def test_creds():
 
 def test_get_access_token(test_creds):
     with patch("requests.post", side_effect=success_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.get_access_token("graph")
-        assert response.is_ok()
-        response = msgraph.get_access_token("audience")
-        assert response.is_ok()
-        response = msgraph.get_access_token("outlook")
-        assert response.is_ok()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.get_access_token("graph")
+        assert response.is_ok
+        response = instance.get_access_token("audience")
+        assert response.is_ok
+        response = instance.get_access_token("outlook")
+        assert response.is_ok
 
 def test_get_access_token_failure(test_creds):
     with patch("requests.post", side_effect=error_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.get_access_token("invalid_mode")
-        assert response.is_err()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.get_access_token("invalid_mode")
+        assert response.is_err
     
 # ---------------------------------------------------------------------------------
 #-------------------------- GET SITE ID TESTS -------------------------------------
 
 def test_get_siteid(test_creds):
     with patch("requests.get", side_effect=success_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.get_siteid("77777777777777777777777", "Communications_site")
-        assert response.is_ok()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.get_siteid("77777777777777777777777", "Communications_site")
+        assert response.is_ok
 
 def test_get_siteid_failure(test_creds):
     with patch("requests.get", side_effect=error_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.get_siteid("WRONG_TOKEN", "Communications_site")
-        assert response.is_err()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.get_siteid("WRONG_TOKEN", "Communications_site")
+        assert response.is_err
 
 # ---------------------------------------------------------------------------------
 #-------------------------- GET DRIVE ID TESTS ------------------------------------
 
 def test_get_driveid(test_creds):
     with patch("requests.get", side_effect=success_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.get_driveid("77777777777777777777777", "CORRECT_SITE_ID")
-        assert response.is_ok()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.get_driveid("77777777777777777777777", "CORRECT_SITE_ID")
+        assert response.is_ok
 
 def test_get_driveid_failure(test_creds):
     with patch("requests.get", side_effect=error_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.get_driveid("WRONG_TOKEN", "WRONG_SITE_ID")
-        assert response.is_err()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.get_driveid("WRONG_TOKEN", "WRONG_SITE_ID")
+        assert response.is_err
 
 # ---------------------------------------------------------------------------------
 #-------------------------- UPLOAD TO DRIVE TESTS ---------------------------------
 
 def test_upload_to_drive(test_creds):
-    with patch("requests.post", side_effect=success_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.upload_to_drive("77777777777777777777777", "CORRECT_DRIVE_ID", "C:\\Users\\User\\Desktop\\test.txt", "testfolder", "text/plain")
-        assert response.is_ok()
+    with patch("requests.put", side_effect=success_response):
+        instance = Msgraph(credentials=test_creds)
+        response = instance.upload_to_drive("77777777777777777777777", "CORRECT_DRIVE_ID", f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt", "testfolder", "text/plain")
+        assert response.is_ok
 
 def test_upload_to_drive_failure(test_creds):
-    with patch("requests.post", side_effect=error_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.upload_to_drive("WRONG_TOKEN", "WRONG_DRIVE_ID", "C:\\Users\\User\\Desktop\\test.txt", "testfolder", "text/plain")
-        assert response.is_err()
+    with patch("requests.put", side_effect=error_response):
+        instance = Msgraph(credentials=test_creds)
+        response = instance.upload_to_drive("WRONG_TOKEN", "WRONG_DRIVE_ID", f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt", "testfolder", "text/plain")
+        assert response.is_err
 
 # ---------------------------------------------------------------------------------
 #-------------------------- SEND EMAIL TESTS --------------------------------------
 
 def test_send_email(test_creds):
     with patch("requests.post", side_effect=success_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.send_email(
+        instance = Msgraph(credentials=test_creds)
+        response = instance.send_email(
             "77777777777777777777777", 
             "About E-mail send tests...", 
             "This e-mail was sent!", 
             ["test@test.com"], 
-            ["C:\\Users\\User\\Desktop\\test.txt"]
+            [f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt"]
             )
-        assert response.is_ok()
+        assert response.is_ok
 
 def test_send_email_failure(test_creds):
     with patch("requests.post", side_effect=error_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.send_email(
+        instance = Msgraph(credentials=test_creds)
+        response = instance.send_email(
             "WRONG_TOKEN", 
             "About E-mail send tests...", 
             "This e-mail was NOT sent!", 
             ["test@test.com"],
-            ["C:\\Users\\User\\Desktop\\test.txt"]
+            [f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt"]
             )
-        assert response.is_err()
+        assert response.is_err
 
 # ---------------------------------------------------------------------------------
 #-------------------------- LIST FILES SHAREPOINT TESTS ---------------------------
 
 def test_list_files_sharepoint(test_creds):
     with patch("requests.get", side_effect=success_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.list_files_sharepoint("77777777777777777777777", "CORRECT_SITE_ID", "CORRECT_DRIVE_ID", "testfolder")
-        assert response.is_ok()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.list_files_sharepoint("77777777777777777777777", "CORRECT_SITE_ID", "CORRECT_DRIVE_ID", "testfolder")
+        assert response.is_ok
 
 def test_list_files_sharepoint_failure(test_creds):
     with patch("requests.get", side_effect=error_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.list_files_sharepoint("WRONG_TOKEN", "WRONG_SITE_ID", "WRONG_DRIVE_ID", "testfolder")
-        assert response.is_err()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.list_files_sharepoint("WRONG_TOKEN", "WRONG_SITE_ID", "WRONG_DRIVE_ID", "testfolder")
+        assert response.is_err
 
 # ---------------------------------------------------------------------------------
 #-------------------------- DOWNLOAD FILE SHAREPOINT TESTS -----------------------
 
 def test_download_file_sharepoint(test_creds):
     with patch("requests.get", side_effect=success_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.download_file_sharepoint("77777777777777777777777", "CORRECT_SITE_ID", "CORRECT_DRIVE_ID", "testfolder", "test.txt", "C:\\Users\\User\\Desktop")
-        assert response.is_ok()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.download_file_sharepoint("77777777777777777777777", "CORRECT_SITE_ID", "CORRECT_DRIVE_ID", "testfolder", "test.txt", f"C:\\Users\\{os.getlogin()}\\Desktop")
+        assert response.is_ok
 
 def test_download_file_sharepoint_failure(test_creds):
     with patch("requests.get", side_effect=error_response):
-        msgraph = msgraph.Msgraph(test_creds)
-        response = msgraph.download_file_sharepoint("WRONG_TOKEN", "WRONG_SITE_ID", "WRONG_DRIVE_ID", "testfolder", "test.txt", "C:\\Users\\User\\Desktop")
-        assert response.is_err()
+        instance = Msgraph(credentials=test_creds)
+        response = instance.download_file_sharepoint("WRONG_TOKEN", "WRONG_SITE_ID", "WRONG_DRIVE_ID", "testfolder", "test.txt", f"C:\\Users\\{os.getlogin()}\\Desktop")
+        assert response.is_err
 
 # ---------------------------------------------------------------------------------
