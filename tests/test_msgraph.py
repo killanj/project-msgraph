6d42d94462345ed966772777655c3f29f40b930e
Johanes Killan <killanalvesj@gmail.com>
2025-05-23 11:19:52 -0300

    Update test_msgraph.py

--- tests/test_msgraph.py
+++ tests/test_msgraph.py
@@ -2,6 +2,7 @@ import os
 import pytest
 from msgraph.msgraph import Msgraph
 from unittest.mock import patch
+import platform
 
 # This is the "tests" file for the project
 # It's the first time I ever wrote any tests, be advised. Any untested cases, open an issue.
@@ -94,6 +95,29 @@ def test_get_driveid_failure(test_creds):
         instance = Msgraph(credentials=test_creds)
         response = instance.get_driveid("WRONG_TOKEN", "WRONG_SITE_ID")
         assert response.is_err
+        
+# ---------------------------------------------------------------------------------
+#-------------------------- DOWNLOAD FILE SHAREPOINT TESTS -----------------------
+
+def test_download_file_sharepoint(test_creds):
+    with patch("requests.get", side_effect=success_response):
+        instance = Msgraph(credentials=test_creds)
+        if platform.system() == "Windows":
+            path = f"C:\\Users\\{os.getlogin()}\\Desktop"
+        else:
+            path = f"/home/{os.getlogin()}/Desktop"
+        response = instance.download_file_sharepoint("77777777777777777777777", "CORRECT_SITE_ID", "CORRECT_DRIVE_ID", "testfolder", "test.txt", path)
+        assert response.is_ok
+
+def test_download_file_sharepoint_failure(test_creds):
+    with patch("requests.get", side_effect=error_response):
+        instance = Msgraph(credentials=test_creds)
+        if platform.system() == "Windows":
+            path = f"C:\\Users\\{os.getlogin()}\\Desktop"
+        else:
+            path = f"/home/{os.getlogin()}/Desktop"
+        response = instance.download_file_sharepoint("WRONG_TOKEN", "WRONG_SITE_ID", "WRONG_DRIVE_ID", "testfolder", "test.txt", path)
+        assert response.is_err
 
 # ---------------------------------------------------------------------------------
 #-------------------------- UPLOAD TO DRIVE TESTS ---------------------------------
@@ -101,13 +125,21 @@ def test_get_driveid_failure(test_creds):
 def test_upload_to_drive(test_creds):
     with patch("requests.put", side_effect=success_response):
         instance = Msgraph(credentials=test_creds)
-        response = instance.upload_to_drive("77777777777777777777777", "CORRECT_DRIVE_ID", f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt", "testfolder", "text/plain")
+        if platform.system() == "Windows":
+            path = f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt"
+        else:
+            path = f"/home/{os.getlogin()}/Desktop/test.txt"
+        response = instance.upload_to_drive("77777777777777777777777", "CORRECT_DRIVE_ID", path, "testfolder", "text/plain")
         assert response.is_ok
 
 def test_upload_to_drive_failure(test_creds):
     with patch("requests.put", side_effect=error_response):
         instance = Msgraph(credentials=test_creds)
-        response = instance.upload_to_drive("WRONG_TOKEN", "WRONG_DRIVE_ID", f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt", "testfolder", "text/plain")
+        if platform.system() == "Windows":
+            path = f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt"
+        else:
+            path = f"/home/{os.getlogin()}/Desktop/test.txt"
+        response = instance.upload_to_drive("WRONG_TOKEN", "WRONG_DRIVE_ID", path, "testfolder", "text/plain")
         assert response.is_err
 
 # ---------------------------------------------------------------------------------
@@ -116,24 +148,32 @@ def test_upload_to_drive_failure(test_creds):
 def test_send_email(test_creds):
     with patch("requests.post", side_effect=success_response):
         instance = Msgraph(credentials=test_creds)
+        if platform.system() == "Windows":
+            path = f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt"
+        else:
+            path = f"/home/{os.getlogin()}/Desktop/test.txt"
         response = instance.send_email(
             "77777777777777777777777", 
             "About E-mail send tests...", 
             "This e-mail was sent!", 
             ["test@test.com"], 
-            [f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt"]
+            [path]
             )
         assert response.is_ok
 
 def test_send_email_failure(test_creds):
     with patch("requests.post", side_effect=error_response):
         instance = Msgraph(credentials=test_creds)
+        if platform.system() == "Windows":
+            path = f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt"
+        else:
+            path = f"/home/{os.getlogin()}/Desktop/test.txt"
         response = instance.send_email(
             "WRONG_TOKEN", 
             "About E-mail send tests...", 
             "This e-mail was NOT sent!", 
             ["test@test.com"],
-            [f"C:\\Users\\{os.getlogin()}\\Desktop\\test.txt"]
+            [path]
             )
         assert response.is_err
 
@@ -150,21 +190,6 @@ def test_list_files_sharepoint_failure(test_creds):
     with patch("requests.get", side_effect=error_response):
         instance = Msgraph(credentials=test_creds)
         response = instance.list_files_sharepoint("WRONG_TOKEN", "WRONG_SITE_ID", "WRONG_DRIVE_ID", "testfolder")
-        assert response.is_err
-
-# ---------------------------------------------------------------------------------
-#-------------------------- DOWNLOAD FILE SHAREPOINT TESTS -----------------------
-
-def test_download_file_sharepoint(test_creds):
-    with patch("requests.get", side_effect=success_response):
-        instance = Msgraph(credentials=test_creds)
-        response = instance.download_file_sharepoint("77777777777777777777777", "CORRECT_SITE_ID", "CORRECT_DRIVE_ID", "testfolder", "test.txt", f"C:\\Users\\{os.getlogin()}\\Desktop")
-        assert response.is_ok
-
-def test_download_file_sharepoint_failure(test_creds):
-    with patch("requests.get", side_effect=error_response):
-        instance = Msgraph(credentials=test_creds)
-        response = instance.download_file_sharepoint("WRONG_TOKEN", "WRONG_SITE_ID", "WRONG_DRIVE_ID", "testfolder", "test.txt", f"C:\\Users\\{os.getlogin()}\\Desktop")
         assert response.is_err
 
 # ---------------------------------------------------------------------------------
